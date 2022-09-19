ecc11011d60fb43f33ea18d1ade32ca5d625eeb2
Sweets Sweetman <sweetmantech@gmail.com>
2022-09-19 10:22:05 -0300

    I add audio player.

--- components/DropSection/DropSection.js
+++ components/DropSection/DropSection.js
@@ -5,38 +5,50 @@ import { ipfsImage } from '@lib/helpers'
 import { maxWidth, border, heroImage } from 'styles/styles.css'
 import Image from 'next/image'
 
-const DropSection = ({ collection }) => (
-  <Box className={maxWidth} p="x4">
-    <Text variant="display-md" mb="x8" align="center">
-      {collection.name}
-    </Text>
-    <Text>{collection?.editionMetadata?.description}</Text>
-    <Box mt="x8" mx="auto" style={{ maxWidth: 560 }}>
-      <Well className={border} p="x6" style={{ borderBottom: 0 }}>
-        <Image
-          className={heroImage}
-          src={ipfsImage(collection.editionMetadata.imageURI)}
-          alt={collection.name}
-          height={500}
-          width={500}
-        />
-      </Well>
-      <Well className={border} p="x6">
-        <Box>
-          {collection != null ? (
-            <>
-              <MintStatus collection={collection} />
-              <MintDetails collection={collection} showPresale={false} />
-            </>
-          ) : (
-            <Paragraph align="center" mt="x8">
-              <SpinnerOG />
-            </Paragraph>
+const DropSection = ({ collection }) => {
+  return (
+    <Box className={maxWidth} p="x4">
+      <Text variant="display-md" mb="x8" align="center">
+        {collection.name}
+      </Text>
+      <Text>{collection?.editionMetadata?.description}</Text>
+      <Box mt="x8" mx="auto" style={{ maxWidth: 560 }}>
+        <Well className={border} p="x6" style={{ borderBottom: 0 }}>
+          <Image
+            className={heroImage}
+            src={ipfsImage(collection.editionMetadata.imageURI)}
+            alt={collection.name}
+            height={500}
+            width={500}
+          />
+          {collection.editionMetadata?.mimeType?.includes?.('audio') && (
+            <audio controls>
+              <source
+                src={ipfsImage(collection.editionMetadata.animationURI)}
+                type={collection.editionMetadata.mimeType}
+              />
+              Your browser does not support the audio element.
+            </audio>
           )}
-        </Box>
-      </Well>
+        </Well>
+
+        <Well className={border} p="x6">
+          <Box>
+            {collection != null ? (
+              <>
+                <MintStatus collection={collection} />
+                <MintDetails collection={collection} showPresale={false} />
+              </>
+            ) : (
+              <Paragraph align="center" mt="x8">
+                <SpinnerOG />
+              </Paragraph>
+            )}
+          </Box>
+        </Well>
+      </Box>
     </Box>
-  </Box>
-)
+  )
+}
 
 export default DropSection
