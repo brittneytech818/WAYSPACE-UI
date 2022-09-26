5c21f875c07a36e5109bfbaadcc5466c3798ae3d
Brittney Spalding <brittney.tech.eng@gmail.com>
2022-09-26 17:19:24 -0300

    MVP: functionally works.

--- components/DropSection/DropSection.js
+++ components/DropSection/DropSection.js
@@ -4,48 +4,95 @@ import { MintDetails } from '@components/MintDetails'
 import { ipfsImage } from '@lib/helpers'
 import { maxWidth, border, heroImage } from 'styles/styles.css'
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import { allChains } from 'wagmi'
+import getDefaultProvider from '@lib/getDefaultProvider'
+import { ethers } from 'ethers'
+import abi from '@lib/WAYSPACE-abi.json'
+import getDrop from '@lib/getDrop'
+import { Spinner } from 'degen'
 
-const DropSection = ({ collection }) => (
-  <Box className={maxWidth} p="x4">
-    <Text variant="display-md" mb="x8" align="center">
-      {collection.name}
-    </Text>
-    <Text>{collection?.editionMetadata?.description}</Text>
-    <Box mt="x8" mx="auto" style={{ maxWidth: 560 }}>
-      <Well className={border} p="x6" style={{ borderBottom: 0 }}>
-        <Image
-          className={heroImage}
-          src={ipfsImage(collection.editionMetadata.imageURI)}
-          alt={collection.name}
-          height={500}
-          width={500}
-        />
-        {collection.editionMetadata?.mimeType?.includes?.('audio') && (
-          <audio controls>
-            <source
-              src={ipfsImage(collection.editionMetadata.animationURI)}
-              type={collection.editionMetadata.mimeType}
-            />
-            Your browser does not support the audio element.
-          </audio>
-        )}
-      </Well>
-      <Well className={border} p="x6">
-        <Box>
-          {collection != null ? (
-            <>
-              <MintStatus collection={collection} />
-              <MintDetails collection={collection} showPresale={false} />
-            </>
-          ) : (
-            <Paragraph align="center" mt="x8">
-              <SpinnerOG />
-            </Paragraph>
+const axios = require('axios').default
+
+const DropSection = ({ trackNumber, saleDetails }) => {
+  const [drop, setDrop] = useState({})
+  const chainId = process.env.NEXT_PUBLIC_CHAIN_ID
+  // Create Ethers Contract
+  const chain = allChains.find((chain) => chain.id.toString() === chainId)
+  const provider = getDefaultProvider(chain.network, chainId)
+
+  useEffect(() => {
+    const load = async () => {
+      const contractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS
+      const contract = new ethers.Contract(contractAddress.toString(), abi, provider)
+      const songURI = await contract.songURI(trackNumber)
+
+      const metadataURI = ipfsImage(songURI)
+      const { data: metadata } = await axios.get(metadataURI)
+      console.log('metadata', metadata)
+
+      // Get Sale Details
+      const drop = getDrop(contract.address, metadata, saleDetails)
+      console.log('NEW DROP', drop)
+      setDrop(drop)
+    }
+
+    if (!drop.name) {
+      load()
+    }
+  }, [trackNumber])
+
+  console.log('DROP', drop)
+
+  if (!drop.editionMetadata)
+    return (
+      <>
+        <h1>rendering wayspace...</h1>
+        <Spinner />
+      </>
+    )
+  return (
+    <Box className={maxWidth} p="x4">
+      <Text variant="display-md" mb="x8" align="center">
+        {drop.name}
+      </Text>
+      <Text>{drop?.editionMetadata?.description}</Text>
+      <Box mt="x8" mx="auto" style={{ maxWidth: 560 }}>
+        <Well className={border} p="x6" style={{ borderBottom: 0 }}>
+          <Image
+            className={heroImage}
+            src={ipfsImage(drop.editionMetadata.imageURI)}
+            alt={drop.name}
+            height={500}
+            width={500}
+          />
+          {drop.editionMetadata?.mimeType?.includes?.('audio') && (
+            <audio controls>
+              <source
+                src={ipfsImage(drop.editionMetadata.animationURI)}
+                type={drop.editionMetadata.mimeType}
+              />
+              Your browser does not support the audio element.
+            </audio>
           )}
-        </Box>
-      </Well>
+        </Well>
+        <Well className={border} p="x6">
+          <Box>
+            {drop != null ? (
+              <>
+                <MintStatus collection={drop} />
+                <MintDetails collection={drop} showPresale={false} />
+              </>
+            ) : (
+              <Paragraph align="center" mt="x8">
+                <SpinnerOG />
+              </Paragraph>
+            )}
+          </Box>
+        </Well>
+      </Box>
     </Box>
-  </Box>
-)
+  )
+}
 
 export default DropSection
--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -11,39 +11,79 @@ import MintBundleButton from '@components/MintBundleButton'
 import { ipfsImage } from '@lib/helpers'
 import { header } from 'styles/styles.css'
 import { useEffect, useState } from 'react'
+import { Contract } from 'ethers'
+import getDrop from '@lib/getDrop'
+import { allChains } from 'wagmi'
+import getDefaultProvider from '@lib/getDefaultProvider'
+import { ethers, utils } from 'ethers'
+import abi from '@lib/WAYSPACE-abi.json'
+import { Spinner } from 'degen'
 
-interface HomePageProps {
-  collections: SubgraphERC721Drop[];
-  chainId?: number;
-}
+const axios = require('axios').default;
 
-const HomePage: NextPage<HomePageProps> = ({ collections, chainId }) => {
+const HomePage: NextPage = () => {
   const [isMobile, setIsMobile] = useState(false);
+  const [drops, setDrops] = useState([]);
+  const [dropCount, setDropCount] = useState(0);
+  const [saleDetails, setSaleDetails] = useState({});
+  
+  const contractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS
+  const chainId = process.env.NEXT_PUBLIC_CHAIN_ID
+  // Create Ethers Contract
+  const chain = allChains.find(
+    (chain) => chain.id.toString() === chainId
+  )
+  const provider = getDefaultProvider(chain.network, chainId);
 
   const handleResize = () => {
     setIsMobile(window?.innerWidth < 720)
   }
   
+
+  useEffect(() => {
+
+    const getDrops = async () => {
+      const contract = new ethers.Contract(contractAddress.toString(), abi, provider);
+      const dropArray = [];
+    
+      const numberOfDrops = await contract.dropsCreated();
+      setDropCount(numberOfDrops);
+      console.log("DROP COUNT", dropCount)
+      const saleDetails = await contract.saleDetails();
+      setSaleDetails(saleDetails)
+      
+      for (let i = 1; i <= numberOfDrops; i++) {
+        dropArray.push(i);
+      }
+      const reversed = dropArray.reverse();
+      console.log("dropArray", reversed)
+      setDrops(reversed)
+    }
+
+    if(drops.length <= 0) {
+      getDrops()
+    }
+  },[contractAddress, provider])
+
   useEffect(() => {
     window.addEventListener("resize", handleResize)
   },[])
-  const ogImage = ipfsImage(collections[0].editionMetadata.imageURI)
 
-  console.log("COLLECTIONS", collections)
+
+  console.log("DROPS", drops)
   return (
     <>
-      <Head ogImage={ogImage}/>
+      <Head ogImage="https://bafybeibp5izlizpzogq72kmeh5twvzdkffxvivcttllgknoccyfvfj7e74.ipfs.nftstorage.link/WELCOME-BACK.jpg" />
       <Flex justify="flex-end" p="x4" className={header}>
         <ConnectWallet />
       </Flex>
       <Stack>
-      <MintBundleButton collection={collections[0]} />
-      <Stack direction={ isMobile ? "column" :"row"} mt="x3" gap="x3">
+      {drops.length > 0 && <MintBundleButton collection={getDrop(contractAddress, {}, saleDetails)} />}
+      <Stack wrap direction={ isMobile ? "column" :"row"} mt="x3" gap="x3">
         {
-          collections.map((song) => 
-            (<DropSection key={song.editionMetadata.trackNumber} collection={song} />)
-
-          )
+          drops.length > 0  && drops.map((dropId) => 
+            (<DropSection key={dropId} trackNumber={dropId} saleDetails={saleDetails} />)
+          ) 
         }
       </Stack>
       </Stack>
