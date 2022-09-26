07404a5d21bc149daad29689bd0b234c00154c11
Brittney Spalding <brittney.tech.eng@gmail.com>
2022-09-26 17:25:44 -0300

    I remove unused imports.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,3 @@
-import { ethers, utils } from 'ethers'
-import { GetStaticProps } from 'next'
-import { ipfsImage } from '@lib/helpers'
-import abi from '@lib/WAYSPACE-abi.json'
-import getDefaultProvider from '@lib/getDefaultProvider'
-import getDrop from '@lib/getDrop'
-import { allChains } from 'wagmi'
 import HomePage from '@components/HomePage/HomePage'
-const axios = require('axios').default;
 
-const MintPage = () => <HomePage />
-export default MintPage;
+export default HomePage;
